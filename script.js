58660d64eee4fbba0a79673805f3df0b8773bd55
email <jjazminclrk@gmail.com>
2020-10-30 20:37:12 -0400

    added URL for open weathermap

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+// variables needed for functions and buttons
 var Search= $("#searchCity");
 var apiKey= "5fee4097b9741407390bbe8f4c7afffc";
+var cityName= $("#cityName");
+var uvIndex= $("#uvIndex");
+var windSpeed= $("#windspeed");
+var temperature= $("#temperature");
+// establishing on clicks
+$(document).ready(function(){
+    getSearches();
+
+    $(".btn").on("click", startSearch);
+
+
+    function startSearch() {
+        var cityName= search.val();
+        var URL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+    }
+})
+
+
